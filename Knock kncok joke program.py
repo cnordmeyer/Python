aa86a020b1a39a9ca81bbc235f25941d9d34c5e3
cnordmeyer <cnordmeyer@students.mchenry.edu>
2024-08-25 16:55:31 -0500

    Add files via upload

---
+++ Knock kncok joke program.py
@@ -0,0 +1,11 @@
+#Knock kncok joke program
+print ("Knock, knock.")
+print ("Who’s there?")
+print ("A broken pencil.")
+print ("A broken pencil who?")
+print ("Never mind. It’s pointless.")
+
+print ("Knock, knock.\n'Who’s there?'\n An extraterrestrial.\n An extraterrestrial who?\n Wait—how many extraterrestrials do you know?")
+
+
+print ("Call JG","Wentworth","877 Cash Now",sep="----!",end="!!!")
