fad6b53cb6f609e14181059b3e2a668122fbcb92
cnordmeyer <cnordmeyer@students.mchenry.edu>
2024-09-02 21:40:25 -0500

    Add files via upload

---
+++ kilogram to pounds program.py
@@ -0,0 +1,19 @@
+Kilogram_1 = 20
+Kilogram_2 = 26
+Kilogram_3 = 74
+Kilogram_4 = 83
+Kilogram_5 = 90
+
+conversion_factor = 2.20462
+
+Result_Pounds1 = Kilogram_1 * conversion_factor
+Result_Pounds2 = Kilogram_2 * conversion_factor
+Result_Pounds3 = Kilogram_3 * conversion_factor
+Result_Pounds4 = Kilogram_4 * conversion_factor
+Result_Pounds5 = Kilogram_5 * conversion_factor
+
+print (f"{Kilogram_1} Kilograms is equal to {Result_Pounds1:.3f} pounds.")
+print (f"{Kilogram_2} Kilograms is equal to {Result_Pounds2:.3f} pounds.")
+print (f"{Kilogram_3} Kilograms is equal to {Result_Pounds3:.3f} pounds.")
+print (f"{Kilogram_4} Kilograms is equal to {Result_Pounds4:.3f} pounds.")
+print (f"{Kilogram_5} Kilograms is equal to {Result_Pounds5:.3f} pounds.")
