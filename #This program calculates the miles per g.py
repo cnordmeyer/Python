903e755ad49cd24d406ee9531f9fd57012581a87
cnordmeyer <cnordmeyer@students.mchenry.edu>
2024-09-02 21:56:39 -0500

    Add files via upload

---
+++ #This program calculates the miles per g.py
@@ -0,0 +1,6 @@
+#This program calculates the miles per gallon of a vehicle.
+
+tank_size = 15 # How many gallons that were used to get to the distance of 600.
+distance = 600 # How many miles it took to use 15 gallons of gas
+mpg = distance / tank_size # This is a formula to find out the miles per gallon, which is equal to the distance divided by the tank size.
+print("Miles per gallon:", mpg)
