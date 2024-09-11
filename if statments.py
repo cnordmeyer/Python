e9bb4ac2779ad59eaf23751e96abb434bd9275ef
cnordmeyer <cnordmeyer@students.mchenry.edu>
2024-09-11 03:26:27 -0500

    Add files via upload

---
+++ if statments.py
@@ -0,0 +1,21 @@
+age=int(input("How old are you"))
+
+if age >= 15:
+    print ("You are old enough to drive")
+else:
+    print ("You are not old enough to drive")
+
+if age >= 18:
+    print ("You can vote")
+else:
+    print ("You cannot vote")
+
+if age >= 21:
+    print ("You can buy alcohol legally")
+else:
+    print ("You cannot buy alcohol legally")
+
+if age >= 55:
+    print ("You are eligible for the senior discount")
+else:
+    print ("You are not eligible for the senior discount")
