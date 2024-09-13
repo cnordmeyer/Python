a20b7feb3003f26168f0eb4300884ddc2e206fdf
cnordmeyer <cnordmeyer@students.mchenry.edu>
2024-09-12 23:34:20 -0500

    Add files via upload

---
+++ 99bottles.py
@@ -0,0 +1,16 @@
+count = 99
+while count >= 2:
+    print(f"{count} bottles of beer on the wall")
+    print(f"{count} bottles of beer")
+    print("Take one down, pass it around")
+    print(f"{count - 1} bottles of beer on the wall!")
+    count -= 1
+count = 1
+while count >= 1:
+    print(f"{count} bottle of beer on the wall")
+    print(f"{count} bottle of beer")
+    print("Take one down, pass it around")
+    print(f"{count - 1} bottle of beer on the wall!")
+    count -= 1
+
+
